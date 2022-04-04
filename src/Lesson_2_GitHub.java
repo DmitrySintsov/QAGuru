36f2e43d7f79da0b3d40aa25294993c880cfc171
DmitrySintsov <sinets@mail.ru>
2022-04-04 20:09:01 +0300

    Comment at master branch

--- src/Lesson_2_GitHub.java
+++ src/Lesson_2_GitHub.java
@@ -3,7 +3,6 @@ public class Lesson_2_GitHub {
     public static void main (String arg[]) {
 
         System.out.println("First program");
-        System.out.println("Release1 comment");
     }
 
 }
