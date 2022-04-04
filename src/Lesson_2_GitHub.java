51154693d30c4f295a20c40b4c1e1830fad216a4
DmitrySintsov <sinets@mail.ru>
2022-04-04 20:24:58 +0300

    Add if

--- src/Lesson_2_GitHub.java
+++ src/Lesson_2_GitHub.java
@@ -3,6 +3,11 @@ public class Lesson_2_GitHub {
     public static void main (String arg[]) {
 
         System.out.println("First program");
+
+        if (5<6) {
+
+            System.out.println("less");
+        }
     }
 
 }
