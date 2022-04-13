b859f6013e3447706c1cc5e71457775a8b1d91d0
DmitrySintsov <sinets@mail.ru>
2022-04-13 13:14:30 +0300

    Add updated project for lesson3

---
+++ Demoqa/src/main/java/com/demoqa/Pages/BasePages.java
@@ -0,0 +1,4 @@
+package com.demoqa.Pages;
+
+public class BasePages {
+}
---
+++ Demoqa/src/test/java/com/demoqa/ui/TestBase.java
@@ -0,0 +1,23 @@
+package com.demoqa.ui;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import com.codeborne.selenide.Configuration;
+
+public class TestBase {
+
+   @BeforeAll
+
+    public static void setUp() {
+
+       Configuration.browser = "chrome";
+       Configuration.baseUrl = "https://demoqa.com";
+       Configuration.timeout = 5000;
+       Configuration.browserSize = "1920x1080";
+
+   }
+
+}
+
+
+
